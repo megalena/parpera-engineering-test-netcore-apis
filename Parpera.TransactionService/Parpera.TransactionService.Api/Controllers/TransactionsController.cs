be52ad9c7a51c3a35de626487f9995795b24462b
Olena Manushyna <olena.manushyna@outlook.com>
2021-04-20 09:29:12 +1000

    Created solution, Api and Domain projects. Created Transaction model and TransactionsController.

---
+++ Parpera.TransactionService/Parpera.TransactionService.Api/Controllers/TransactionsController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Parpera.TransactionService.Domain;
+using System;
+using System.Collections.Generic;
+
+namespace Parpera.TransactionService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionsController : ControllerBase
+    {
+        // GET: api/<TransactionsController>
+        [HttpGet]
+        public IEnumerable<Transaction> Get()
+        {
+            return new Transaction[]
+            {
+                new Transaction()
+                {
+                    Id = 1, DateTime = DateTime.Parse("2020-03-30 23:53:23"), Description = "Bank Deposit", Amount = 500M, Status = TransactionStatus.Completed
+                }
+            };
+        }
+    }
+}
---
+++ Parpera.TransactionService/Parpera.TransactionService.Domain/Transaction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Parpera.TransactionService.Domain
+{
+    public class Transaction
+    {
+        public int Id { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public string Description { get; set; }
+
+        public decimal Amount { get; set; }
+
+        public TransactionStatus Status { get; set; }
+    }
+}
